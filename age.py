62b19a71287d749d3c96064a15772fe1203ccbfd
SM123-byte <mailswagat@gmail.com>
2025-08-30 18:06:16 +0100

    Add files via upload

---
+++ age.py
@@ -0,0 +1,13 @@
+# Age Checker 
+
+age = int(input("Enter your age: "))
+if age >= 10:
+    if age <= 20:
+        print("You are in the age group range of 10 - 20 years old!")
+    else:
+        print("You are over the age of 20!")
+else:
+    print("You are under the age of 10!")
+
+
+
